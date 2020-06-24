a4e1a13400d7a68aaf1968d2e470f91478424775
John Pham <mr.john.pham@gmail.com>
2020-06-24 11:27:18 -0400

    Use Mongo client

--- server.js
+++ server.js
@@ -17,79 +17,159 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug')
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) {
-        console.log('Database error: ' + err);
-    } else {
-        console.log('Successful database connection');
-      
-        app.use(session({
-          secret: process.env.SESSION_SECRET,
-          resave: true,
-          saveUninitialized: true,
-        }));
-        app.use(passport.initialize());
-        app.use(passport.session());
-      
-        function ensureAuthenticated(req, res, next) {
-          if (req.isAuthenticated()) {
-              return next();
-          }
-          res.redirect('/');
-        };
+mongo
+  .connect(process.env.DATABASE, {useUnifiedTopology: true})
+  .then(client => {
+      console.log(`Successful database connection`);
 
-        passport.serializeUser((user, done) => {
-          done(null, user.id);
+      app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+      }));
+      app.use(passport.initialize());
+      app.use(passport.session());
+    
+      function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('/');
+      };
+
+      passport.serializeUser((user, done) => {
+        done(null, user.id);
+      });
+
+      passport.deserializeUser((id, done) => {
+          db.collection('socialusers').findOne(
+              {id: id},
+              (err, doc) => {
+                  done(null, doc);
+              }
+          );
+      });
+
+    
+      /*
+      *  ADD YOUR CODE BELOW
+      */
+    
+    
+    
+    
+    
+    
+    
+      /*
+      *  ADD YOUR CODE ABOVE
+      */
+    
+    
+      app.route('/')
+        .get((req, res) => {
+          res.render(process.cwd() + '/views/pug/index');
         });
 
-        passport.deserializeUser((id, done) => {
-            db.collection('socialusers').findOne(
-                {id: id},
-                (err, doc) => {
-                    done(null, doc);
-                }
-            );
+      app.route('/profile')
+        .get(ensureAuthenticated, (req, res) => {
+              res.render(process.cwd() + '/views/pug/profile', {user: req.user});
         });
 
-      
-        /*
-        *  ADD YOUR CODE BELOW
-        */
-      
-      
-      
-      
-      
-      
-      
-        /*
-        *  ADD YOUR CODE ABOVE
-        */
-      
-      
-        app.route('/')
-          .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index');
-          });
-
-        app.route('/profile')
-          .get(ensureAuthenticated, (req, res) => {
-               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
-          });
-
-        app.route('/logout')
-          .get((req, res) => {
-              req.logout();
-              res.redirect('/');
-          });
-
-        app.use((req, res, next) => {
-          res.status(404)
-            .type('text')
-            .send('Not Found');
+      app.route('/logout')
+        .get((req, res) => {
+            req.logout();
+            res.redirect('/');
         });
+
+      app.use((req, res, next) => {
+        res.status(404)
+          .type('text')
+          .send('Not Found');
+      });
+    
+      app.listen(process.env.PORT || 3000, () => {
+        console.log("Listening on port " + process.env.PORT);
+      });  
+    })
+    .catch(err => {
+      console.log('Database error: ' + err);
+    })
+// }
+
+// mongo.connect(process.env.DATABASE, (err, db) => {
+//     if(err) {
+//         console.log('Database error: ' + err);
+//     } else {
+//         console.log('Successful database connection');
       
-        app.listen(process.env.PORT || 3000, () => {
-          console.log("Listening on port " + process.env.PORT);
-        });  
-}});
+//         app.use(session({
+//           secret: process.env.SESSION_SECRET,
+//           resave: true,
+//           saveUninitialized: true,
+//         }));
+//         app.use(passport.initialize());
+//         app.use(passport.session());
+      
+//         function ensureAuthenticated(req, res, next) {
+//           if (req.isAuthenticated()) {
+//               return next();
+//           }
+//           res.redirect('/');
+//         };
+
+//         passport.serializeUser((user, done) => {
+//           done(null, user.id);
+//         });
+
+//         passport.deserializeUser((id, done) => {
+//             db.collection('socialusers').findOne(
+//                 {id: id},
+//                 (err, doc) => {
+//                     done(null, doc);
+//                 }
+//             );
+//         });
+
+      
+//         /*
+//         *  ADD YOUR CODE BELOW
+//         */
+      
+      
+      
+      
+      
+      
+      
+//         /*
+//         *  ADD YOUR CODE ABOVE
+//         */
+      
+      
+//         app.route('/')
+//           .get((req, res) => {
+//             res.render(process.cwd() + '/views/pug/index');
+//           });
+
+//         app.route('/profile')
+//           .get(ensureAuthenticated, (req, res) => {
+//                res.render(process.cwd() + '/views/pug/profile', {user: req.user});
+//           });
+
+//         app.route('/logout')
+//           .get((req, res) => {
+//               req.logout();
+//               res.redirect('/');
+//           });
+
+//         app.use((req, res, next) => {
+//           res.status(404)
+//             .type('text')
+//             .send('Not Found');
+//         });
+      
+//         app.listen(process.env.PORT || 3000, () => {
+//           console.log("Listening on port " + process.env.PORT);
+//         });  
+// }});
